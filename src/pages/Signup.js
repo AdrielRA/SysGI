2c05aff08feb521f40a0c0352ce665ae53916fc7
AdrielRA <adrieldeveloper@hotmail.com>
2020-01-21 06:48:18 -0300

    preAlpha 2.0.1

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,12 +1,44 @@
 import React, { useState } from 'react';
 import { View, KeyboardAvoidingView , Text, TextInput,TouchableHighlight, Picker } from 'react-native';
+import { Stitch, AnonymousCredential } from "mongodb-stitch-react-native-sdk";
 import { LinearGradient } from 'expo-linear-gradient';
 import Styles from '../styles/styles';
 import Colors from '../styles/colors';
 
 function Signup({navigation}) {
-  const [categoria, setCategoria] = useState(null);
+  const mongoClient = Stitch.defaultAppClient;
+
+  const [categoria, setCategoria] = useState(0);
   const CategoriaChanged = (categoria) => { setCategoria(categoria); };
+  const [nome, setNome] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [email, setEmail] = useState('');
+  const [confEmail, setConfEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [confSenha, setConfSenha] = useState('');
+
+  const _saveUser = (user) => {
+    
+    if(!mongoClient.auth.isLoggedIn){
+      mongoClient.auth.loginWithCredential(new AnonymousCredential())
+      .then(user_ => {
+        console.log(`Successfully logged in as user ${user_.id}`);
+        this.setState({ currentUserId: user_.id });
+      })
+      .catch(err => {
+        console.log(`Failed to log in anonymously: ${err}`);
+        this.setState({ currentUserId: undefined });
+      });
+    }
+
+    if(mongoClient.auth.isLoggedIn){
+      mongoClient.callFunction("add_user", [user]).then(salvar =>{
+        console.log(`Resultado: ${salvar.msg}`);
+        if(salvar.res){ navigation.goBack(); }
+      });
+    }
+     
+  };
 
   return (
     <LinearGradient
@@ -22,58 +54,64 @@ function Signup({navigation}) {
             selectedValue={categoria}
             onValueChange={(itemValue, itemIndex) => CategoriaChanged(itemValue)}>
             <Picker.Item label="Categoria" value={null} />
-            <Picker.Item label="Professor" value="Professor"/>
-            <Picker.Item label="Advogado" value="Advogado"/>
-            <Picker.Item label="Policial" value="Policial"/>
-            <Picker.Item label="Delegado" value="Delegado"/>
-            <Picker.Item label="Promotor" value="Promotor"/>
-            <Picker.Item label="Juiz" value="Juiz"/>
+            <Picker.Item label="Professor" value="1"/>
+            <Picker.Item label="Advogado" value="2"/>
+            <Picker.Item label="Policial" value="3"/>
+            <Picker.Item label="Delegado" value="4"/>
+            <Picker.Item label="Promotor" value="5"/>
+            <Picker.Item label="Juiz" value="6"/>
           </Picker>
         </View>
         <TextInput
             placeholder="Nome de Usuário"
             placeholderTextColor={Colors.Terciary.White}
             style={Styles.campo}
+            onChangeText={(nome_) => setNome(nome_)}
         />
         <TextInput
             placeholder="Telefone"
             placeholderTextColor={Colors.Terciary.White}
             keyboardType="phone-pad"
             style={Styles.campo}
+            onChangeText={(telefone_) => setTelefone(telefone_)}
         />
         <TextInput
           placeholder="emai@email.com"
           placeholderTextColor={Colors.Terciary.White}
           keyboardType="email-address"
           style={Styles.campo}
+          onChangeText={(email_) => setEmail(email_)}
         />
         <TextInput
           placeholder="emai.confirma@email.com"
           placeholderTextColor={Colors.Terciary.White}
           keyboardType="email-address"
           style={Styles.campo}
+          onChangeText={(confEmail_) => setConfEmail(confEmail_)}
         />
         <TextInput
           placeholder="Senha"
           placeholderTextColor={Colors.Terciary.White}
           secureTextEntry={true}
           style={Styles.campo}
+          onChangeText={(senha_) => setSenha(senha_)}
         />
         <TextInput
           placeholder="Confirma Senha"
           placeholderTextColor={Colors.Terciary.White}
           secureTextEntry={true}
           style={Styles.campo}
+          onChangeText={(confSenha_) => setConfSenha(confSenha_)}
         />
         <View style={{flexDirection: 'row', justifyContent:"center", alignItems:"center"}}>
           <TouchableHighlight style={Styles.btnTransparent}
             underlayColor={'transparent'}
-            onPress={() =>  navigation.goBack()}>
+            onPress={() => navigation.goBack()}>
             <Text style={Styles.btnTextTransparent}>VOLTAR</Text>
           </TouchableHighlight>
           <TouchableHighlight style={Styles.btnSecundary}
             underlayColor={Colors.Primary.White}
-            onPress={() =>  navigation.goBack()}>
+            onPress={() => _saveUser({Nome:nome, Email:email, Telefone:telefone, Credencial:Number(categoria), Passpassword:senha}) }>
             <Text style={Styles.btnTextSecundary}>SALVAR</Text>
           </TouchableHighlight>
         </View>
